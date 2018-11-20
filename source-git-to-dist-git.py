2d20f5df37169be94bcf2e99126e3d23a7ee5060
lachmanfrantisek <flachman@redhat.com>
2018-11-20 10:30:34 +0100

    Add pulling of the dist-git and creating patches
    Signed-off-by: lachmanfrantisek <flachman@redhat.com>

---
+++ source-git-to-dist-git.py
@@ -0,0 +1,34 @@
+import shutil
+
+import click
+
+from transformator import Transformator
+
+
+@click.command("sg2dg")
+@click.option("--upstream-ref")
+@click.option("--package-name")
+@click.option("--dest-dir")
+@click.argument("repo")
+@click.argument("dist-git")
+@click.argument("name")
+@click.argument("version")
+def sg2dg(upstream_ref, package_name, dest_dir, repo, dist_git, name, version):
+    upstream_ref = upstream_ref or version
+    try:
+        with Transformator(url=repo,
+                           upstream_name=name,
+                           package_name=package_name,
+                           version=version,
+                           dest_dir=dest_dir) as t:
+            t.clone_dist_git_repo(dist_git_url=dist_git)
+            t.create_archive()
+            t.create_patches(upstream=upstream_ref)
+            click.echo(f"DEST_DIR: {t.dest_dir}")
+    finally:
+        print(f"Cleaning: {t.dest_dir}")
+        shutil.rmtree(t.dest_dir)
+
+
+if __name__ == '__main__':
+    sg2dg()
