3e812c40113fce5883930fe48ab77b298ae99b8d
Tomas Tomecek <ttomecek@redhat.com>
2019-01-09 12:43:38 +0100

    rewrite fedmsg consuming
    I am leaving code for fedora-messaging on purpose
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- sourcegit/cli/watch_fedora_ci.py
+++ sourcegit/cli/watch_fedora_ci.py
@@ -4,17 +4,10 @@ This bot will listen on fedmsg for finished CI runs and will update respective s
 import logging
 
 import click
-import fedmsg
 
 from sourcegit.api import SourceGitAPI
 from sourcegit.config import get_context_settings
 
-
-package_mapping = {
-    "python-docker": {
-        "source-git": "TomasTomecek/docker-py-source-git"
-    }
-}
 
 logger = logging.getLogger(__name__)
 
@@ -27,10 +20,6 @@ def watcher(message_id):
 
     :return: int, retcode
     """
-    # we can watch for runs directly:
-    # "org.centos.prod.ci.pipeline.allpackages.complete"
-    topic = "org.fedoraproject.prod.pagure.pull-request.flag.added"
-
     a = SourceGitAPI()
 
     if message_id:
@@ -39,6 +28,4 @@ def watcher(message_id):
             a.process_ci_result(fedmsg_dict)
             return
     else:
-        logger.info("listening on fedmsg, topic=%s", topic)
-        for name, endpoint, topic, msg in fedmsg.tail_messages(topic=topic):
-            a.process_ci_result(msg)
+        a.keep_fwding_ci_results()
