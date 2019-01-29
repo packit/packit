e24bb3b6bbb99ee4f81d23dea0e3e6180a43c013
Frantisek Lachman <flachman@redhat.com>
2019-01-29 10:01:28 +0100

    Fix PR sync
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- sourcegit/sync.py
+++ sourcegit/sync.py
@@ -7,20 +7,20 @@ from functools import lru_cache
 
 import git
 
-from onegittorulethemall.services.pagure import PagureService
-
-from sourcegit.constants import dg_pr_key_sg_commit, dg_pr_key_sg_pr
+from ogr.services.pagure import PagureService
+from sourcegit.constants import DG_PR_COMMENT_KEY_SG_PR, DG_PR_COMMENT_KEY_SG_COMMIT
 from sourcegit.downstream_checks import get_check_by_name
 from sourcegit.transformator import Transformator, get_package_mapping
 from sourcegit.utils import commits_to_nice_str
 from sourcegit.watcher import SourceGitCheckHelper
 
-
 logger = logging.getLogger(__name__)
 
 
 class Synchronizer:
-    def __init__(self, github_token, pagure_user_token, pagure_package_token, pagure_fork_token) -> None:
+    def __init__(
+            self, github_token, pagure_user_token, pagure_package_token, pagure_fork_token
+    ) -> None:
         self.github_token = github_token
         self.pagure_user_token = pagure_user_token
         self.pagure_package_token = pagure_package_token
@@ -31,7 +31,7 @@ class Synchronizer:
         #        on the class
         self._tempdirs = []
 
-    def reset_checks(self, full_name: str, pr_id: str, checks_list: list):
+    def reset_checks(self, full_name: str, pr_id: int, checks_list: list):
         """
         Before syncing a new change downstream, we need to reset status of checks for all the configured tests
         and wait for testing systems to get us the new ones.
@@ -74,10 +74,14 @@ class Synchronizer:
             nice_msg = json.dumps(fedmsg_dict, indent=4)
             logger.debug(f"Processing fedmsg:\n{nice_msg}")
             return self.sync(
-                target_url=fedmsg_dict["msg"]["pull_request"]["base"]["repo"]["html_url"],
+                target_url=fedmsg_dict["msg"]["pull_request"]["base"]["repo"][
+                    "html_url"
+                ],
                 target_ref=fedmsg_dict["msg"]["pull_request"]["base"]["ref"],
                 source_ref=fedmsg_dict["msg"]["pull_request"]["head"]["ref"],
-                full_name=fedmsg_dict["msg"]["pull_request"]["head"]["repo"]["full_name"],
+                full_name=fedmsg_dict["msg"]["pull_request"]["base"]["repo"][
+                    "full_name"
+                ],
                 top_commit=fedmsg_dict["msg"]["pull_request"]["head"]["sha"],
                 pr_id=fedmsg_dict["msg"]["pull_request"]["number"],
                 pr_url=fedmsg_dict["msg"]["pull_request"]["html_url"],
@@ -119,7 +123,6 @@ class Synchronizer:
         repo = self.get_repo(url=target_url)
         self.checkout_pr(repo=repo, pr_id=pr_id)
 
-        # FIXME: branch name should be tied to the sg PR, so something like this: f"source-git-{pr_id}"
         with Transformator(
                 url=target_url,
                 repo=repo,
@@ -130,7 +133,8 @@ class Synchronizer:
         ) as transformator:
             transformator.clone_dist_git_repo()
 
-            dist_git_new_branch = transformator.dist_git_repo.create_head(source_ref)
+            dist_git_branch_name = f"source-git-{pr_id}"
+            dist_git_new_branch = transformator.dist_git_repo.create_head(dist_git_branch_name)
             dist_git_new_branch.checkout()
 
             transformator.create_archive()
@@ -152,22 +156,24 @@ class Synchronizer:
 
             project = pagure.get_project(repo=package_name, namespace="rpms")
 
-            if not project.fork:
+            project_fork = project.get_fork()
+            if not project_fork:
                 logger.info("Creating a fork.")
                 project.fork_create()
+                project_fork = project.get_fork()
 
-            is_push_force = source_ref in project.fork.branches
             transformator.dist_git_repo.create_remote(
-                name="origin-fork", url=project.fork.git_urls["ssh"]
+                name="origin-fork", url=project_fork.get_git_urls()["ssh"]
             )
             # I suggest to comment this one while testing when the push is not needed
             transformator.dist_git_repo.remote("origin-fork").push(
-                refspec=source_ref, force=is_push_force
+                refspec=dist_git_branch_name,
+                force=dist_git_branch_name in project_fork.get_branches()
             )
 
             self.reset_checks(full_name, pr_id, package_config["checks"])
             self._update_or_create_dist_git_pr(project, pr_id, pr_url, top_commit, title,
-                                               source_ref)
+                                               source_ref=dist_git_branch_name)
 
     @lru_cache()
     def get_repo(self, url, directory=None):
@@ -190,10 +196,74 @@ class Synchronizer:
         repo.remote().fetch(refspec=f"pull/{pr_id}/head:pull/{pr_id}")
         repo.refs[f"pull/{pr_id}"].checkout()
 
+    def _update_or_create_dist_git_pr(self, project, pr_id, pr_url, top_commit, title, source_ref):
+        # Sadly, pagure does not support editing initial comments of a PR via the API
+        # https://pagure.io/pagure/issue/4111
+        # Short-term solution: keep adding comments
+        # and get updated info about sg PR ID and commit desc
+        for pr in project.get_pr_list():
+
+            sg_pr_id_match = project.search_in_pr(
+                pr_id=pr.id,
+                filter_regex=DG_PR_COMMENT_KEY_SG_PR + r":\s*(\d+)",
+                reverse=True,
+                description=True,
+            )
+            if not sg_pr_id_match:
+                logger.debug(f"No automation comment found in dist-git PR: {pr.id}.")
+                continue
+
+            sg_pr_id = sg_pr_id_match[1]
+            if sg_pr_id_match[1] != str(pr_id):
+                logger.debug(f"Dist-git PR `{pr.id}` does not match "
+                             f"source-git PR `{pr_id}`.")
+                continue
+
+            commit_match = project.search_in_pr(
+                pr_id=pr.id,
+                filter_regex=DG_PR_COMMENT_KEY_SG_COMMIT + r":\s*(\d+)",
+                reverse=True,
+                description=True,
+            )
+            if not commit_match:
+                logger.debug(f"Dist-git PR `{pr.id}` does not contain top-commit of the "
+                             f"source-git PR `{pr_id}`.")
+                continue
+
+            logger.debug(f"Adding a new comment with update to existing PR.")
+            msg = (
+                f"New changes were pushed to the upstream pull request\n\n"
+                f"[{DG_PR_COMMENT_KEY_SG_PR}: {pr_id}]({pr_url})\n"
+                f"{DG_PR_COMMENT_KEY_SG_COMMIT}: {top_commit}"
+            )
+            # FIXME: consider storing the data above as a git note of the top commit
+            project.change_token(self.pagure_package_token)
+            project.pr_comment(pr.id, msg)
+            logger.info("new comment added on PR %s", sg_pr_id)
+            break
+        else:
+            logger.debug(f"Matching dist-git PR not found => creating a new one.")
+            msg = (
+                f"This pull request contains changes from upstream "
+                f"and is meant to integrate them into Fedora\n\n"
+                f"[{DG_PR_COMMENT_KEY_SG_PR}: {pr_id}]({pr_url})\n"
+                f"{DG_PR_COMMENT_KEY_SG_COMMIT}: {top_commit}"
+            )
+            # This pagure call requires token from the package's FORK
+            project_fork = project.get_fork()
+            project_fork.change_token(self.pagure_fork_token)
+            dist_git_pr_id = project_fork.pr_create(
+                title=f"[source-git] {title}",
+                body=msg,
+                source_branch=source_ref,
+                target_branch="master",
+            ).id
+            logger.info(f"PR created: {dist_git_pr_id}")
+
     def clean(self):
         while self._tempdirs:
             tempdir = self._tempdirs.pop()
-            logger.debug(f"Cleaning: {tempdir }")
+            logger.debug(f"Cleaning: {tempdir}")
             shutil.rmtree(tempdir)
 
     def __enter__(self):
