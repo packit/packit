c5ef71cc98cf8cf9b54d32c1b460d95720777fc2
Frantisek Lachman <flachman@redhat.com>
2019-02-25 11:21:18 +0100

    Pass config to CLI subcommands
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/cli/update.py
+++ packit/cli/update.py
@@ -7,20 +7,18 @@ import logging
 import click
 
 from packit.api import PackitAPI
-from packit.config import pass_config, get_context_settings, get_local_package_config
-from packit.local_project import LocalProject
-from packit.transformator import Transformator
+from packit.config import pass_config, get_context_settings
 
 logger = logging.getLogger(__file__)
 
 
 @click.command("propose-update", context_settings=get_context_settings())
-@click.option("--dist-git-branch",
-              help="Target branch in dist-git to release into.",
-              default="master"
-              )
-@click.option("--dist-git-path",
-              help="Path to dist-git repo to work in.")
+@click.option(
+    "--dist-git-branch",
+    help="Target branch in dist-git to release into.",
+    default="master",
+)
+@click.option("--dist-git-path", help="Path to dist-git repo to work in.")
 @pass_config
 def update(config, dist_git_path, dist_git_branch):
     """
