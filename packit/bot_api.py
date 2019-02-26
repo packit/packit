42b0eaed86bb3fb3a5b549df55044a6bb823deea
Frantisek Lachman <flachman@redhat.com>
2019-02-26 14:27:13 +0100

    Add structure of the bot_api
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/bot_api.py
+++ packit/bot_api.py
@@ -5,50 +5,67 @@ and other tools working with multiple packages.
 This API is built on top of the packit.api.
 """
 import logging
-from typing import Dict, Any
+from typing import Dict, Optional
 
-import requests
-
+from packit.api import PackitAPI
+from packit.config import Config, PackageConfig
 from packit.fed_mes_consume import Consumerino
+from packit.watcher import SourceGitCheckHelper
 
 logger = logging.getLogger(__name__)
 
 
-class PackitBotApi:
-    def __init__(self) -> None:
-        # TODO: the url template should be configurable
-        self.datagrepper_url = (
-            "https://apps.fedoraproject.org/datagrepper/id?id={msg_id}&is_raw=true"
-        )
+class PackitBotAPI:
+    def __init__(self, config: Config) -> None:
+        self.config = config
         self.consumerino = Consumerino()
 
     def watch_upstream_pull_request(self):
-        pass
+        for topic, action, msg in self.consumerino.iterate_pull_requests():
+            if action in ["opened", "synchronize", "reopened"]:
+                self.sync_upstream_pull_request_with_fedmsg(fedmsg=msg)
 
-    def consume_upstream_pull_request(self, fedmsg: Dict):
-        pass
+    def sync_upstream_pull_request_with_fedmsg(self, fedmsg: Dict):
+        package_config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        self.sync_upstream_pull_request(
+            package_config=package_config, pr_id=0, dist_git_branch=""
+        )
 
-    def watch_fedora_ci(self):
-        pass
-
-    def consume_fedora_ci(self, fedmsg: Dict):
-        pass
+    def sync_upstream_pull_request(
+        self, package_config: PackageConfig, pr_id: int, dist_git_branch: str
+    ):
+        logger.info("syncing the upstream code to downstream")
+        packit_api = PackitAPI(config=self.config, package_config=package_config)
+        packit_api.sync_pr(pr_id=pr_id, dist_git_branch=dist_git_branch)
 
     def watch_upstream_release(self):
-        pass
+        for topic, msg in self.consumerino.iterate_releases():
+            self.sync_upstream_release_with_fedmsg(fedmsg=msg)
 
-    def consume_upstream_release(self, fedmsg: Dict):
-        pass
+    def sync_upstream_release_with_fedmsg(self, fedmsg: Dict):
+        config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        self.sync_upstream_release(
+            package_config=config, version="", dist_git_branch=""
+        )
 
-    def _fetch_fedmsg_dict(self, msg_id: str) -> Dict[str, Any]:
-        """
-        Fetch selected message from datagrepper
+    def sync_upstream_release(
+        self,
+        package_config: PackageConfig,
+        version: Optional[str],
+        dist_git_branch: str,
+    ):
+        logger.info("syncing the upstream code to downstream")
+        packit_api = PackitAPI(config=self.config, package_config=package_config)
+        packit_api.update(dist_git_branch=dist_git_branch)
 
-        :param msg_id: str
-        :return: dict, the fedmsg
-        """
-        logger.debug(f"Proccessing message: {msg_id}")
-        url = self.datagrepper_url.format(msg_id=msg_id)
-        response = requests.get(url)
-        msg_dict = response.json()
-        return msg_dict
+    def watch_fedora_ci(self):
+        for topic, msg in self.consumerino.iterate_dg_pr_flags():
+            self.sync_fedora_ci_with_fedmsg(fedmsg=msg)
+
+    def sync_fedora_ci_with_fedmsg(self, fedmsg: Dict):
+        package_config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        self.sync_fedora_ci(package_config=package_config)
+
+    def sync_fedora_ci(self, package_config: PackageConfig):
+        sg = SourceGitCheckHelper(config=self.config, package_config=package_config)
+        sg.process_new_dg_flag(None)
