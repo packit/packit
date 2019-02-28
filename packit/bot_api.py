210496f0b82d2cfae4b3a7cd03b3ed559fc7d76f
Frantisek Lachman <flachman@redhat.com>
2019-02-28 07:39:08 +0100

    Import BotAPI
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/bot_api.py
+++ packit/bot_api.py
@@ -5,10 +5,12 @@ and other tools working with multiple packages.
 This API is built on top of the packit.api.
 """
 import logging
+from functools import lru_cache
 from typing import Dict, Optional
 
+from ogr.services.github import GithubService
 from packit.api import PackitAPI
-from packit.config import Config, PackageConfig
+from packit.config import Config, PackageConfig, get_packit_config_from_repo
 from packit.fed_mes_consume import Consumerino
 from packit.watcher import SourceGitCheckHelper
 
@@ -20,13 +22,35 @@ class PackitBotAPI:
         self.config = config
         self.consumerino = Consumerino()
 
+    @property
+    @lru_cache()
+    def _github_service(self):
+        return GithubService(token=self.config.github_token)
+
     def watch_upstream_pull_request(self):
         for topic, action, msg in self.consumerino.iterate_pull_requests():
             if action in ["opened", "synchronize", "reopened"]:
                 self.sync_upstream_pull_request_with_fedmsg(fedmsg=msg)
 
     def sync_upstream_pull_request_with_fedmsg(self, fedmsg: Dict):
-        package_config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        repo_name = fedmsg["msg"]["pull_request"]["head"]["repo"]["name"]
+        namespace = fedmsg["msg"]["pull_request"]["head"]["repo"]["owner"]["login"]
+        ref = fedmsg["msg"]["pull_request"]["head"]["ref"]
+        pr_id = fedmsg["msg"]["pull_request"]["id"]
+
+        github_repo = self._github_service.get_project(
+            repo=repo_name, namespace=namespace
+        )
+
+        package_config = get_packit_config_from_repo(
+            sourcegit_project=github_repo, ref=ref
+        )
+
+        if not package_config:
+            logger.debug(
+                f"No packit config: skipping pull-request {pr_id} for {namespace}/{repo_name}."
+            )
+            return
         self.sync_upstream_pull_request(
             package_config=package_config, pr_id=0, dist_git_branch=""
         )
@@ -39,13 +63,38 @@ class PackitBotAPI:
         packit_api.sync_pr(pr_id=pr_id, dist_git_branch=dist_git_branch)
 
     def watch_upstream_release(self):
+        """
+        Listen on fedmsg and sync the upstream releases to the upstream pull-request.
+        """
         for topic, msg in self.consumerino.iterate_releases():
             self.sync_upstream_release_with_fedmsg(fedmsg=msg)
 
     def sync_upstream_release_with_fedmsg(self, fedmsg: Dict):
-        config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        """
+        Sync the upstream release to the distgit pull-request.
+
+        :param fedmsg: fedmsg dict
+        """
+        repo_name = fedmsg["msg"]["repository"]["name"]
+        namespace = fedmsg["msg"]["repository"]["owner"]["login"]
+        version = fedmsg["msg"]["release"]["tag_name"]
+
+        github_repo = self._github_service.get_project(
+            repo=repo_name, namespace=namespace
+        )
+
+        package_config = get_packit_config_from_repo(
+            sourcegit_project=github_repo, ref=version
+        )
+
+        if not package_config:
+            logger.debug(
+                f"No packit config: skipping release {version} for {namespace}/{repo_name}."
+            )
+            return
+
         self.sync_upstream_release(
-            package_config=config, version="", dist_git_branch=""
+            package_config=package_config, version=version, dist_git_branch=""
         )
 
     def sync_upstream_release(
@@ -54,17 +103,24 @@ class PackitBotAPI:
         version: Optional[str],
         dist_git_branch: str,
     ):
+        """
+        Sync the upstream release to the distgit pull-request.
+
+        :param package_config: PackageConfig
+        :param version: not used now, str
+        :param dist_git_branch: str
+        """
         logger.info("syncing the upstream code to downstream")
         packit_api = PackitAPI(config=self.config, package_config=package_config)
-        packit_api.update(dist_git_branch=dist_git_branch)
+        packit_api.sync_release(dist_git_branch=dist_git_branch, version=version)
 
     def watch_fedora_ci(self):
         for topic, msg in self.consumerino.iterate_dg_pr_flags():
             self.sync_fedora_ci_with_fedmsg(fedmsg=msg)
 
     def sync_fedora_ci_with_fedmsg(self, fedmsg: Dict):
-        package_config = PackageConfig.get_config_from_fedmsg(fedmsg)
-        self.sync_fedora_ci(package_config=package_config)
+        # package_config = PackageConfig.get_config_from_fedmsg(fedmsg)
+        self.sync_fedora_ci(package_config=None)
 
     def sync_fedora_ci(self, package_config: PackageConfig):
         sg = SourceGitCheckHelper(config=self.config, package_config=package_config)
