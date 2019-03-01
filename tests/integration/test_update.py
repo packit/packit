cb6b8074ae7b3ca34783ba3f58f5afe0ba26d731
Jiri Popelka <jpopelka@redhat.com>
2019-03-01 16:04:57 +0100

    [tests] os -> pathlib

--- tests/integration/test_update.py
+++ tests/integration/test_update.py
@@ -1,4 +1,4 @@
-import os
+from os import chdir
 
 from rebasehelper.specfile import SpecFile
 
@@ -11,12 +11,12 @@ def test_basic_local_update(beer, mock_update_workflow):
     """ basic propose-update test: mock remote API, use local upstream and dist-git """
     u, d = beer
 
-    os.chdir(u)
+    chdir(u)
     c = Config()
     c.dist_git_path = d
     p = PackitAPI(c)
     p.update("master")
 
-    assert os.path.exists(os.path.join(d, TARBALL_NAME))
-    spec = SpecFile(os.path.join(d, "beer.spec"), None)
+    assert (d / TARBALL_NAME).is_file()
+    spec = SpecFile(str(d / "beer.spec"), None)
     assert spec.get_full_version() == "0.1.0"
