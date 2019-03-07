d0f24d636cdc614f1b229cac118a5f73220022e7
Frantisek Lachman <flachman@redhat.com>
2019-03-07 11:36:14 +0100

    Ad more exceptions to @cover_packit_exception
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/cli/utils.py
+++ packit/cli/utils.py
@@ -1,16 +1,24 @@
 import functools
+import logging
 import sys
+
+import click
 
 from packit.exceptions import PackitException
 
+logger = logging.getLogger(__name__)
 
-def cover_packit_exception(_func=None, *, exit_code=1):
+
+def cover_packit_exception(_func=None, *, exit_code=None):
     """
     Decorator for executing the function in the try-except block.
 
     The PackitException is caught and
     - raised in debug
     - sys.exit(exit_code), otherwise
+
+    On other Exceptions we print the message about creating an issue.
+
 
     If the function receives config, it recognises the debug mode.
     => use it after the @pass_config decorator
@@ -24,10 +32,27 @@ def cover_packit_exception(_func=None, *, exit_code=1):
                     func(config, *args, **kwargs)
                 else:
                     func(*args, **kwargs)
-            except PackitException:
+            except KeyboardInterrupt:
+                click.echo("Quitting on user request.")
+                sys.exit(1)
+            except PackitException as exc:
                 if config and config.debug:
-                    raise
-                sys.exit(exit_code)
+                    logger.exception(exc)
+                else:
+                    logger.error(exc)
+                sys.exit(exit_code or 2)
+            except Exception as exc:
+                if config and config.debug:
+                    logger.exception(exc)
+                else:
+                    logger.error(exc)
+                    click.echo(
+                        "Unexpected exception occurred,\n"
+                        "please fill an issue here:\n"
+                        "https://github.com/packit-service/packit/issues",
+                        err=True,
+                    )
+                sys.exit(exit_code or 3)
 
         return covered_func
 
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,6 +1,5 @@
 import sys
 
-import pytest
 from flexmock import flexmock
 
 from packit.cli.utils import cover_packit_exception
@@ -17,7 +16,64 @@ def test_cover_packit_exception_decorator():
     covered_func()
 
 
-def test_cover_packit_exception_decorator_exit_code():
+def test_cover_packit_exception_decorator_attribute():
+    flexmock(sys).should_receive("exit")
+
+    @cover_packit_exception
+    def covered_func(attribute):
+        assert attribute
+        raise PackitException("Other test exception")
+
+    covered_func(attribute=flexmock())
+
+
+def test_cover_packit_exception_decorator_exit_code_default():
+    flexmock(sys).should_receive("exit").with_args(2)
+
+    @cover_packit_exception()
+    def covered_func():
+        raise PackitException("Test exception")
+
+    covered_func()
+
+
+def test_cover_packit_exception_decorator_exit_code_keyboard_interrupt():
+    flexmock(sys).should_receive("exit").with_args(1)
+
+    @cover_packit_exception()
+    def covered_func():
+        raise KeyboardInterrupt("Test keyboard interrupt")
+
+    covered_func()
+
+
+def test_cover_packit_exception_decorator_exit_code_other_exception():
+    flexmock(sys).should_receive("exit").with_args(3)
+
+    class CustomException(Exception):
+        pass
+
+    @cover_packit_exception()
+    def covered_func():
+        raise CustomException("Test keyboard interrupt")
+
+    covered_func()
+
+
+def test_cover_packit_exception_decorator_exit_code_other_exception_override():
+    flexmock(sys).should_receive("exit").with_args(5)
+
+    class CustomException(Exception):
+        pass
+
+    @cover_packit_exception(exit_code=5)
+    def covered_func():
+        raise CustomException("Test keyboard interrupt")
+
+    covered_func()
+
+
+def test_cover_packit_exception_decorator_exit_code_override():
     flexmock(sys).should_receive("exit").with_args(5)
 
     @cover_packit_exception(exit_code=5)
@@ -38,28 +94,26 @@ def test_cover_packit_exception_decorator_config_debug_false():
 
 
 def test_cover_packit_exception_decorator_config_debug_true():
-    flexmock(sys).should_receive("exit").times(0)
+    flexmock(sys).should_receive("exit")
 
     @cover_packit_exception
     def covered_func(config=None):
         raise PackitException("Test exception")
 
-    with pytest.raises(PackitException):
-        covered_func(config=flexmock(debug=True))
+    covered_func(config=flexmock(debug=True))
 
 
 def test_cover_packit_exception_decorator_other_exception():
-    flexmock(sys).should_receive("exit")
+    flexmock(sys).should_receive("exit").with_args(3)
 
     class CustomException(Exception):
         pass
 
     @cover_packit_exception
-    def covered_func(config=None):
+    def covered_func():
         raise CustomException("Other test exception")
 
-    with pytest.raises(CustomException):
-        covered_func()
+    covered_func()
 
 
 def test_cover_packit_exception_decorator_other_exception_config_debug():
@@ -72,5 +126,4 @@ def test_cover_packit_exception_decorator_other_exception_config_debug():
     def covered_func(config=None):
         raise CustomException("Other test exception")
 
-    with pytest.raises(CustomException):
-        covered_func(config=flexmock(debug=True))
+    covered_func(config=flexmock(debug=True))
