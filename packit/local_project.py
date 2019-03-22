8ed3bbde4303c8d25de280d45f3fe5a09699297d
Frantisek Lachman <flachman@redhat.com>
2019-03-22 15:35:10 +0100

    Add refresh to LocalProject
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/local_project.py
+++ packit/local_project.py
@@ -44,6 +44,7 @@ class LocalProject:
         repo_name: str = None,
         path_or_url: str = None,
         offline: bool = False,
+        refresh=True,
     ) -> None:
 
         self.working_dir_temporary = False
@@ -62,26 +63,10 @@ class LocalProject:
         self.full_name = full_name
         self.repo_name = repo_name
         self.namespace = namespace
+        self.offline = offline
 
-        change = True
-        while change:
-            # we are trying to get new information while it is possible
-            # new iteration is done only if there was a change in the last iteration
-
-            change = (
-                self._parse_repo_name_full_name_and_namespace()
-                or self._parse_git_repo_from_working_dir(offline=offline)
-                or self._parse_git_project_from_repo_namespace_and_git_project(offline)
-                or self._parse_git_service_from_git_project(offline)
-                or self._parse_ref_from_git_repo()
-                or self._parse_working_dir_from_git_repo()
-                or self._parse_git_repo_from_git_url(offline)
-                or self._parse_git_url_from_git_project(offline)
-                or self._parse_repo_name_from_git_project()
-                or self._parse_namespace_from_git_project()
-                or self._parse_git_url_from_git_repo()
-                or self._parse_namespace_from_git_repo()
-            )
+        if refresh:
+            self.refresh_the_arguments()
 
         if ref:
             # If ref was specified, we will checkout that
@@ -101,6 +86,33 @@ class LocalProject:
             return self._get_ref_from_git_repo()
         return None
 
+    def clean(self):
+        if self.working_dir_temporary:
+            logger.debug(f"Cleaning: {self.working_dir}")
+            shutil.rmtree(self.working_dir)
+            self.working_dir_temporary = False
+
+    def refresh_the_arguments(self):
+        change = True
+        while change:
+            # we are trying to get new information while it is possible
+            # new iteration is done only if there was a change in the last iteration
+
+            change = (
+                self._parse_repo_name_full_name_and_namespace()
+                or self._parse_git_repo_from_working_dir()
+                or self._parse_git_project_from_repo_namespace_and_git_project()
+                or self._parse_git_service_from_git_project()
+                or self._parse_ref_from_git_repo()
+                or self._parse_working_dir_from_git_repo()
+                or self._parse_git_repo_from_git_url()
+                or self._parse_git_url_from_git_project()
+                or self._parse_repo_name_from_git_project()
+                or self._parse_namespace_from_git_project()
+                or self._parse_git_url_from_git_repo()
+                or self._parse_namespace_from_git_url()
+            )
+
     def _is_url(self, path_or_url):
         try:
             res = requests.head(path_or_url)
@@ -110,84 +122,6 @@ class LocalProject:
                 logger.warning("path_or_url is nor directory nor url")
         except requests.exceptions.BaseHTTPError:
             logger.warning("path_or_url is nor directory nor url")
-        return False
-
-    def _parse_namespace_from_git_repo(self):
-        if self.git_repo and not self.namespace:
-            self.namespace = get_namespace(self.git_url)
-            if self.namespace:
-                return True
-        return False
-
-    def _parse_git_url_from_git_repo(self):
-        if self.git_repo and not self.git_url:
-            # this is prone to errors
-            # also if we want url to upstream, we may want to ask for it explicitly
-            # since this can point to a fork
-            # .urls returns generator
-            self.git_url = list(self.git_repo.remote().urls)[0]
-            logger.debug(f"remote url of the repo is {self.git_url}")
-            return True
-        return False
-
-    def _parse_namespace_from_git_project(self):
-        if self.git_project and not self.namespace:
-            self.namespace = self.git_project.namespace
-            return True
-        return False
-
-    def _parse_repo_name_from_git_project(self):
-        if self.git_project and not self.repo_name:
-            self.repo_name = self.git_project.repo
-            return True
-        return False
-
-    def _parse_git_url_from_git_project(self, offline):
-        if self.git_project and not self.git_url and not offline:
-            self.git_url = self.git_project.get_git_urls()["git"]
-            return True
-        return False
-
-    def _parse_git_repo_from_git_url(self, offline):
-        if self.git_url and not self.working_dir and not self.git_repo and not offline:
-            self.git_repo = get_repo(url=self.git_url)
-            self.working_dir_temporary = True
-            return True
-        return False
-
-    def _parse_working_dir_from_git_repo(self):
-        if self.git_repo and not self.working_dir:
-            self.working_dir = self.git_repo.working_dir
-            return True
-        return False
-
-    def _parse_ref_from_git_repo(self):
-        if self.git_repo and not self._ref:
-            self._ref = self._get_ref_from_git_repo()
-            return self._ref is not None
-        return False
-
-    def _parse_git_service_from_git_project(self, offline):
-        if self.git_project and not self.git_service and not offline:
-            self.git_service = self.git_project.service
-            return True
-        return False
-
-    def _parse_git_project_from_repo_namespace_and_git_project(
-        self, offline: bool
-    ) -> bool:
-
-        if (
-            self.repo_name
-            and self.namespace
-            and self.git_service
-            and not self.git_project
-            and not offline
-        ):
-            self.git_project = self.git_service.get_project(
-                repo=self.repo_name, namespace=self.namespace
-            )
-            return True
         return False
 
     def _parse_repo_name_full_name_and_namespace(self):
@@ -203,7 +137,7 @@ class LocalProject:
             change = True
         return change
 
-    def _parse_git_repo_from_working_dir(self, offline: bool) -> bool:
+    def _parse_git_repo_from_working_dir(self) -> bool:
         """
         Get the repo from the self.working_dir (clone self.git_url if it is not a git repo)
         """
@@ -214,7 +148,7 @@ class LocalProject:
                 self.git_repo = git.Repo(path=self.working_dir)
                 return True
 
-            elif self.git_url and not offline:
+            elif self.git_url and not self.offline:
                 logger.debug(
                     "we just cloned git repo %s to %s", self.git_url, self.working_dir
                 )
@@ -223,11 +157,86 @@ class LocalProject:
 
         return False
 
-    def clean(self):
-        if self.working_dir_temporary:
-            logger.debug(f"Cleaning: {self.working_dir}")
-            shutil.rmtree(self.working_dir)
-            self.working_dir_temporary = False
+    def _parse_git_project_from_repo_namespace_and_git_project(self,) -> bool:
+
+        if (
+            self.repo_name
+            and self.namespace
+            and self.git_service
+            and not self.git_project
+            and not self.offline
+        ):
+            self.git_project = self.git_service.get_project(
+                repo=self.repo_name, namespace=self.namespace
+            )
+            return True
+        return False
+
+    def _parse_git_service_from_git_project(self):
+        if self.git_project and not self.git_service and not self.offline:
+            self.git_service = self.git_project.service
+            return True
+        return False
+
+    def _parse_ref_from_git_repo(self):
+        if self.git_repo and not self._ref:
+            self._ref = self._get_ref_from_git_repo()
+            return self._ref is not None
+        return False
+
+    def _parse_working_dir_from_git_repo(self):
+        if self.git_repo and not self.working_dir:
+            self.working_dir = self.git_repo.working_dir
+            return True
+        return False
+
+    def _parse_git_repo_from_git_url(self):
+        if (
+            self.git_url
+            and not self.working_dir
+            and not self.git_repo
+            and not self.offline
+        ):
+            self.git_repo = get_repo(url=self.git_url)
+            self.working_dir_temporary = True
+            return True
+        return False
+
+    def _parse_git_url_from_git_project(self):
+        if self.git_project and not self.git_url and not self.offline:
+            self.git_url = self.git_project.get_git_urls()["git"]
+            return True
+        return False
+
+    def _parse_repo_name_from_git_project(self):
+        if self.git_project and not self.repo_name:
+            self.repo_name = self.git_project.repo
+            return True
+        return False
+
+    def _parse_namespace_from_git_project(self):
+        if self.git_project and not self.namespace:
+            self.namespace = self.git_project.namespace
+            return True
+        return False
+
+    def _parse_git_url_from_git_repo(self):
+        if self.git_repo and not self.git_url:
+            # this is prone to errors
+            # also if we want url to upstream, we may want to ask for it explicitly
+            # since this can point to a fork
+            # .urls returns generator
+            self.git_url = list(self.git_repo.remote().urls)[0]
+            logger.debug(f"remote url of the repo is {self.git_url}")
+            return True
+        return False
+
+    def _parse_namespace_from_git_url(self):
+        if self.git_url and not self.namespace:
+            self.namespace = get_namespace(self.git_url)
+            if self.namespace:
+                return True
+        return False
 
     def _get_ref_from_git_repo(self):
         if self.git_repo.head.is_detached:
