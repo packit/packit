2af6f49baa343350894f49b494eae7845ee892e3
Petr "Stone" Hracek <phracek@redhat.com>
2019-04-02 13:10:01 +0200

    Fix test
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/integration/test_distgit.py
+++ tests/integration/test_distgit.py
@@ -10,7 +10,7 @@ from packit.exceptions import PackitException
 def test_distgit_commit_empty(distgit_instance):
     d, dg = distgit_instance
     with pytest.raises(PackitException) as ex:
-        dg.commit(dg.local_project, "", "")
+        dg.commit("", "")
     assert (
         str(ex.value)
         == "No changes are present in the dist-git repo: nothing to commit."
