12746912773d726b95fb34fdb9a12cbc10902317
Frantisek Lachman <flachman@redhat.com>
2019-04-04 11:08:34 +0200

    Do not call pytest fixtures directly
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- tests/unit/test_base_git.py
+++ tests/unit/test_base_git.py
@@ -2,6 +2,7 @@ import pytest
 from flexmock import flexmock
 
 from packit import utils
+from packit.base_git import PackitRepositoryBase
 from packit.config import PackageConfig, Config
 from packit.distgit import DistGit
 
@@ -30,6 +31,18 @@ def upstream_with_actions():
     )
 
 
+@pytest.fixture()
+def packit_repository_base():
+    return PackitRepositoryBase(
+        config=flexmock(),
+        package_config=flexmock(
+            PackageConfig(
+                actions={"action-a": "command --a", "action-b": "command --b"}
+            )
+        ),
+    )
+
+
 def test_has_action_upstream(upstream_with_actions):
     assert upstream_with_actions.has_action("action-a")
     assert not upstream_with_actions.has_action("action-c")
@@ -40,67 +53,46 @@ def test_has_action_distgit(distgit_with_actions):
     assert not distgit_with_actions.has_action("action-c")
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_with_action_non_defined(base_git_fixture):
-    base_git = base_git_fixture()
-
-    if base_git.with_action(action_name="unknown-action"):
+def test_with_action_non_defined(packit_repository_base):
+    if packit_repository_base.with_action(action_name="unknown-action"):
         # this is the style we are using that function
         return
 
     assert False
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_with_action_defined(base_git_fixture):
+def test_with_action_defined(packit_repository_base):
     flexmock(utils).should_receive("run_command").once()
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
-    if base_git.with_action(action_name="action-a"):
+    if packit_repository_base.with_action(action_name="action-a"):
         # this is the style we are using that function
         assert False
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_with_action_working_dir(base_git_fixture):
+def test_with_action_working_dir(packit_repository_base):
     flexmock(utils).should_receive("run_command").with_args(
         cmd="command --a", cwd="my/working/dir"
     ).once()
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
-    assert not base_git.with_action(action_name="action-a")
+    assert not packit_repository_base.with_action(action_name="action-a")
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_run_action_hook_not_defined(base_git_fixture):
+def test_run_action_hook_not_defined(packit_repository_base):
     flexmock(utils).should_receive("run_command").times(0)
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
-    base_git.run_action(action_name="not-defined")
+    packit_repository_base.run_action(action_name="not-defined")
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_run_action_not_defined(base_git_fixture):
+def test_run_action_not_defined(packit_repository_base):
     flexmock(utils).should_receive("run_command").times(0)
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
     action_method = (
         flexmock()
@@ -111,19 +103,17 @@ def test_run_action_not_defined(base_git_fixture):
         .action_function
     )
 
-    base_git.run_action("not-defined", action_method, "arg", kwarg="kwarg")
+    packit_repository_base.run_action(
+        "not-defined", action_method, "arg", kwarg="kwarg"
+    )
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_run_action_defined(base_git_fixture):
+def test_run_action_defined(packit_repository_base):
     flexmock(utils).should_receive("run_command").with_args(
         cmd="command --a", cwd="my/working/dir"
     ).once()
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
     action_method = (
         flexmock()
@@ -134,17 +124,13 @@ def test_run_action_defined(base_git_fixture):
         .action_function
     )
 
-    base_git.run_action("action-a", action_method, "arg", kwarg="kwarg")
+    packit_repository_base.run_action("action-a", action_method, "arg", kwarg="kwarg")
 
 
-@pytest.mark.parametrize(
-    "base_git_fixture", [distgit_with_actions, upstream_with_actions]
-)
-def test_get_output_from_action_not_defined(base_git_fixture):
+def test_get_output_from_action_not_defined(packit_repository_base):
     flexmock(utils).should_receive("run_command").times(0)
 
-    base_git = base_git_fixture()
-    base_git._local_project = flexmock(working_dir="my/working/dir")
+    packit_repository_base.local_project = flexmock(working_dir="my/working/dir")
 
-    result = base_git.get_output_from_action("not-defined")
+    result = packit_repository_base.get_output_from_action("not-defined")
     assert result is None
