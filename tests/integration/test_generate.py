60e38d31cc825eb4ee30a9ad061142d184c78257
Radoslav Pitonak <rado.pitonak@gmail.com>
2019-04-08 12:50:31 +0200

    add tests for generate

---
+++ tests/integration/test_generate.py
@@ -0,0 +1,27 @@
+from os import chdir
+
+from tests.spellbook import call_packit
+
+
+def test_generate_pass(upstream_without_config):
+    u = upstream_without_config
+    chdir(u)
+
+    assert not (u / ".packit.yaml").is_file()
+
+    # This test requires packit on pythonpath
+    result = call_packit(parameters=["generate"])
+
+    assert result.exit_code == 0
+
+    assert (u / ".packit.yaml").is_file()
+
+
+def test_generate_fail(upstream_n_distgit):
+    u, d = upstream_n_distgit
+    chdir(u)
+
+    # This test requires packit on pythonpath
+    result = call_packit(parameters=["generate"])
+
+    assert result.exit_code == 2  # packit config already exists --force needed
