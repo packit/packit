c6d3705a83aca149a2b4d56e8fe26603731fa0c2
Tomas Tomecek <ttomecek@redhat.com>
2019-04-15 14:11:40 +0200

    [packit] sync from downstream branch 'master'
    Downstream commit: e45f51fd481039a8f527451944a2feb4816ccebc
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

---
+++ fedora-tests/upstream.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+set -ex
+
+PROJECT_NAME=packit
+ORG=packit-service
+PACKAGE_NAME=python3-$PROJECT_NAME
+GITHUB=https://github.com/$ORG/$PROJECT_NAME.git
+
+WHERETOUNPACK=$PROJECT_NAME
+FIND_VERSION=`rpm -q $PACKAGE_NAME | sed -r "s/.*-$PROJECT_NAME-(.*)\.[a-zA-Z0-9_]+$/\1/"`
+VERSION="${FIND_VERSION%-*}"
+
+git clone $GITHUB $WHERETOUNPACK
+(
+    cd $WHERETOUNPACK
+    git checkout tags/$VERSION
+)
+cp -rf $WHERETOUNPACK/tests tests
+rm -rf $WHERETOUNPACK
+
+PYTHONPATH="$PYTHONPATH:." pytest-3 -v tests
