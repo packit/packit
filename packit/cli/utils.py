558c88eea3918600b592492a06451465032aa502
Tomas Tomecek <ttomecek@redhat.com>
2019-07-22 12:25:27 +0200

    have a dedicated var in packageconfig for local dg clone
    Fixes #422
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- packit/cli/utils.py
+++ packit/cli/utils.py
@@ -110,7 +110,7 @@ def get_packit_api(
     )
 
     if dist_git_path:
-        package_config._downstream_project_url = dist_git_path
+        package_config.dist_git_clone_path = dist_git_path
     package_config.upstream_project_url = local_project.working_dir
 
     api = PackitAPI(
