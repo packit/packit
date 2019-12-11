b44148baf6091de4a8437ac49f752ab568544f20
Jiri Popelka <jpopelka@redhat.com>
2019-12-11 10:36:53 +0100

    Revert "Marshmallow schema implementation"

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -25,7 +25,6 @@ from pathlib import Path
 
 import pytest
 from flexmock import flexmock
-from marshmallow import ValidationError
 
 from ogr import GithubService, PagureService
 from ogr.abstract import GitProject, GitService
@@ -41,6 +40,7 @@ from packit.config import (
     SyncFilesItem,
 )
 from packit.config.aliases import get_build_targets, get_branches
+from packit.exceptions import PackitInvalidConfigException
 
 
 def get_job_config_dict_simple():
@@ -101,10 +101,9 @@ def test_job_config_not_equal(job_config_simple, job_config_full):
 
 
 def test_job_config_blah():
-    with pytest.raises(ValidationError) as ex:
+    with pytest.raises(PackitInvalidConfigException) as ex:
         JobConfig.get_from_dict({"job": "asdqwe", "trigger": "salt"})
-    assert "'trigger': ['Invalid enum member salt']" in str(ex.value)
-    assert "'job': ['Invalid enum member asdqwe']" in str(ex.value)
+    assert "'asdqwe' is not one of " in str(ex.value)
 
 
 @pytest.mark.parametrize(
@@ -121,10 +120,10 @@ def test_job_config_blah():
 )
 def test_job_config_validate(raw, is_valid):
     if is_valid:
-        JobConfig.get_from_dict(raw)
+        JobConfig.validate(raw)
     else:
-        with pytest.raises(ValidationError):
-            JobConfig.get_from_dict(raw)
+        with pytest.raises(PackitInvalidConfigException):
+            JobConfig.validate(raw)
 
 
 @pytest.mark.parametrize(
@@ -292,21 +291,19 @@ def test_package_config_not_equal(not_equal_package_config):
 )
 def test_package_config_validate(raw, is_valid):
     if not is_valid:
-        with pytest.raises(ValidationError):
-            PackageConfig.get_from_dict(raw)
+        with pytest.raises(PackitInvalidConfigException):
+            PackageConfig.validate(raw)
     else:
-        PackageConfig.get_from_dict(raw)
+        PackageConfig.validate(raw)
 
 
 @pytest.mark.parametrize(
     "raw",
     [
         {},
-        {"specfile_path": "test/spec/file/path", "something": "different"},
-        {
-            "specfile_path": "test/spec/file/path",
-            "jobs": [{"trigger": "release", "release_to": ["f28"]}],
-        },
+        {"something": "different"},
+        {"synced_files": ["fedora/package.spec", "somefile"]},
+        {"jobs": [{"trigger": "release", "release_to": ["f28"]}]},
     ],
 )
 def test_package_config_parse_error(raw):
@@ -392,6 +389,7 @@ def test_package_config_parse_error(raw):
                 "specfile_path": "fedora/package.spec",
                 "synced_files": ["fedora/package.spec", "somefile"],
                 "jobs": [get_job_config_dict_full()],
+                "something": "stupid",
                 "downstream_package_name": "package",
             },
             PackageConfig(
@@ -413,6 +411,7 @@ def test_package_config_parse_error(raw):
                 "specfile_path": "fedora/package.spec",
                 "synced_files": ["fedora/package.spec"],
                 "jobs": [get_job_config_dict_full()],
+                "something": "stupid",
                 "upstream_project_url": "https://github.com/asd/qwe",
                 "upstream_package_name": "qwe",
                 "dist_git_base_url": "https://something.wicked",
@@ -442,6 +441,7 @@ def test_package_config_parse_error(raw):
                     "get-current-version": "get-me-version",
                 },
                 "jobs": [],
+                "something": "stupid",
                 "upstream_project_url": "https://github.com/asd/qwe",
                 "upstream_package_name": "qwe",
                 "dist_git_base_url": "https://something.wicked",
