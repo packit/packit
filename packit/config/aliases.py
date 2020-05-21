349bda0a9bf1346535e3d1ff8169dd6889486bf3
Hunor Csomortáni <csomh@redhat.com>
2020-05-21 07:46:27 +0200

    Introduce the 'epel-all' alias
    Works similar to 'fedora-all', and aliases 'epel-[6..8]'.
    
    Signed-off-by: Hunor Csomortáni <csomh@redhat.com>

--- packit/config/aliases.py
+++ packit/config/aliases.py
@@ -27,6 +27,7 @@ ALIASES: Dict[str, List[str]] = {
     "fedora-development": ["fedora-rawhide"],
     "fedora-stable": ["fedora-30", "fedora-31", "fedora-32"],
     "fedora-all": ["fedora-rawhide", "fedora-30", "fedora-31", "fedora-32"],
+    "epel-all": ["epel-6", "epel-7", "epel-8"],
 }
 ARCHITECTURE_LIST: List[str] = [
     "aarch64",
