366ba443c5db5086a08f01bad6cde094aef5ce93
Jan Ščotka <jscotka@redhat.com>
2020-05-22 15:41:32 +0200

    fix the tests according to new version of requre

--- tests_recording/testbase.py
+++ tests_recording/testbase.py
@@ -1,11 +1,9 @@
-import inspect
 import os
 import shutil
-import unittest
 from subprocess import check_output, CalledProcessError
 
 from requre.helpers.tempfile import TempFile
-from requre.storage import PersistentObjectStorage
+from requre.base_testclass import RequreTestCase
 
 import packit.distgit
 import packit.upstream
@@ -19,7 +17,7 @@ PERSISTENT_DATA_PREFIX = os.path.join(
 )
 
 
-class PackitUnittestOgr(unittest.TestCase):
+class PackitUnittestOgr(RequreTestCase):
     @staticmethod
     def get_test_config():
         try:
@@ -28,16 +26,6 @@ class PackitUnittestOgr(unittest.TestCase):
             conf = Config()
         conf.dry_run = True
         return conf
-
-    def get_datafile_filename(self, suffix="yaml"):
-        prefix = PERSISTENT_DATA_PREFIX
-        test_file_name = os.path.basename(inspect.getfile(self.__class__)).rsplit(
-            ".", 1
-        )[0]
-        test_class_name = f"{self.id()}.{suffix}"
-        testdata_dirname = os.path.join(prefix, test_file_name)
-        os.makedirs(testdata_dirname, mode=0o777, exist_ok=True)
-        return os.path.join(testdata_dirname, test_class_name)
 
     def set_git_user(self):
         try:
@@ -49,10 +37,8 @@ class PackitUnittestOgr(unittest.TestCase):
             check_output(["git", "config", "--global", "user.name", "Tester"])
 
     def setUp(self):
+        super().setUp()
         self.conf = self.get_test_config()
-        response_file = self.get_datafile_filename()
-        PersistentObjectStorage().storage_file = response_file
-        PersistentObjectStorage().dump_after_store = True
         self.static_tmp = "/tmp/packit_tmp"
         os.makedirs(self.static_tmp, exist_ok=True)
         TempFile.root = self.static_tmp
@@ -70,5 +56,5 @@ class PackitUnittestOgr(unittest.TestCase):
         )
 
     def tearDown(self):
-        PersistentObjectStorage().dump()
+        super().tearDown()
         shutil.rmtree(self.static_tmp)
