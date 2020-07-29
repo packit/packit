0cf9481ef37d78d074445a80ee013f9113f99a38
Matej Focko <mfocko@redhat.com>
2020-07-29 12:29:55 +0200

    Allow backward compatibility for all `utils` funcs
    Adds `set_logging` import from `packit.utils.logging` to `packit.utils`
    to make it backward compatible with tests in `packit-service`.
    
    Shall be removed after updating the import in `packit-service`, kept
    for tests.

--- packit/utils/__init__.py
+++ packit/utils/__init__.py
@@ -19,3 +19,40 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from packit.utils.commands import run_command, run_command_remote, cwd
+from packit.utils.extensions import get_rev_list_kwargs, assert_existence, nested_get
+from packit.utils.logging import (
+    StreamLogger,
+    PackitFormatter,
+    set_logging,
+    commits_to_nice_str,
+)
+from packit.utils.repo import (
+    is_git_repo,
+    get_repo,
+    get_namespace_and_repo_name,
+    is_a_git_ref,
+    git_remote_url_to_https_url,
+)
+from packit.utils.version import get_packit_version
+
+
+__all__ = [
+    run_command.__name__,
+    run_command_remote.__name__,
+    cwd.__name__,
+    get_rev_list_kwargs.__name__,
+    assert_existence.__name__,
+    nested_get.__name__,
+    StreamLogger.__name__,
+    PackitFormatter.__name__,
+    set_logging.__name__,
+    commits_to_nice_str.__name__,
+    is_git_repo.__name__,
+    get_repo.__name__,
+    get_namespace_and_repo_name.__name__,
+    is_a_git_ref.__name__,
+    git_remote_url_to_https_url.__name__,
+    get_packit_version.__name__,
+]
