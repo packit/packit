4262e2a3b59ed54b1f67020e4aa90753a3087459
lbarcziova <lbarczio@redhat.com>
2020-09-15 13:19:04 +0200

    Use correct arguments in sync_release() from tests

--- tests/integration/test_source_git.py
+++ tests/integration/test_source_git.py
@@ -51,7 +51,9 @@ def test_basic_local_update_without_patching(
     distgit, _ = distgit_and_remote
     mock_spec_download_remote_s(distgit)
 
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref="0.1.0")
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref="0.1.0"
+    )
 
     assert (distgit / TARBALL_NAME).is_file()
     spec = Specfile(distgit / "beer.spec")
@@ -69,7 +71,9 @@ def test_basic_local_update_empty_patch(
 
     distgit, _ = distgit_and_remote
     mock_spec_download_remote_s(distgit)
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref=ref)
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref=ref
+    )
 
     assert (distgit / TARBALL_NAME).is_file()
     spec = Specfile(distgit / "beer.spec")
@@ -106,7 +110,9 @@ def test_basic_local_update_patch_content(
     source_file.write_text(" And I am sad.")
     git_add_and_commit(directory=sourcegit, message="make a file sad")
 
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref="0.1.0")
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref="0.1.0"
+    )
 
     git_diff = subprocess.check_output(
         ["git", "diff", "HEAD~", "HEAD"], cwd=distgit
@@ -270,7 +276,9 @@ def test_basic_local_update_patch_content_with_metadata(
     source_file.write_text(" And I am sad.")
     git_add_and_commit(directory=sourcegit, message="make a file sad")
 
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref="0.1.0")
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref="0.1.0"
+    )
 
     git_diff = subprocess.check_output(
         ["git", "diff", "HEAD~", "HEAD"], cwd=distgit
@@ -323,7 +331,9 @@ def test_basic_local_update_patch_content_with_metadata_and_patch_ignored(
     source_file.write_text(" And I am sad.")
     git_add_and_commit(directory=sourcegit, message="make a file sad")
 
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref="0.1.0")
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref="0.1.0"
+    )
 
     git_diff = subprocess.check_output(
         ["git", "diff", "HEAD~", "HEAD"], cwd=distgit
@@ -364,7 +374,9 @@ def test_basic_local_update_patch_content_with_downstream_patch(
     source_file.write_text(" And I am sad.")
     git_add_and_commit(directory=sourcegit, message="make a file sad")
 
-    api_instance_source_git.sync_release("master", "0.1.0", upstream_ref="0.1.0")
+    api_instance_source_git.sync_release(
+        dist_git_branch="master", version="0.1.0", upstream_ref="0.1.0"
+    )
 
     git_diff = subprocess.check_output(
         ["git", "diff", "HEAD~", "HEAD"], cwd=distgit
