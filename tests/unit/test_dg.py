355ed7d2859bfa24ccca12467c80fe2a5a16ef4e
lbarcziova <lbarczio@redhat.com>
2020-10-20 13:19:32 +0200

    Check description, regenerate test responses

--- tests/unit/test_dg.py
+++ tests/unit/test_dg.py
@@ -10,25 +10,50 @@ from packit.local_project import LocalProject
 
 
 @pytest.mark.parametrize(
-    "title, branch, prs, exists",
+    "title, description, branch, prs, exists",
     [
-        ("Update", "f31", [flexmock(title="Update", target_branch="f31")], True),
-        ("Update", "f32", [flexmock(title="Update", target_branch="f31")], False),
         (
-            "Something else",
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
             "f31",
-            [flexmock(title="Update", target_branch="f31")],
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                )
+            ],
+            True,
+        ),
+        (
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+            "f31",
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 8957453b",
+                )
+            ],
             False,
         ),
         (
-            "Something else",
+            "Update",
+            "Upstream tag: 0.4.0\nUpstream commit: 6957453b",
             "f32",
-            [flexmock(title="Update", target_branch="f31")],
+            [
+                flexmock(
+                    title="Update",
+                    target_branch="f31",
+                    description="Upstream tag: 0.4.0\nUpstream commit: 6957453b",
+                )
+            ],
             False,
         ),
     ],
 )
-def test_pr_exists(title, branch, prs, exists):
+def test_pr_exists(title, description, branch, prs, exists):
     local_project = LocalProject(
         git_project=flexmock(service="something", get_pr_list=lambda: prs),
         refresh=False,
@@ -38,4 +63,4 @@ def test_pr_exists(title, branch, prs, exists):
         package_config=flexmock(PackageConfig()),
         local_project=local_project,
     )
-    assert distgit.pr_exists(title, branch) == exists
+    assert distgit.pr_exists(title, description, branch) == exists
