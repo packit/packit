485ee0a6f7701430dcf38b1e0791cd2d35f5c340
Jan Sakalos <sakalosj@gmail.com>
2020-10-26 10:19:41 +0100

    Implement fallback_return_value decorator
    Function of fallback_return_value is to return fallback value in case of
    specific exception is raised during function/method run.

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -30,6 +30,7 @@ from pkg_resources import DistributionNotFound, Distribution
 
 from packit.api import get_packit_version
 from packit.exceptions import PackitException, ensure_str
+from packit.utils.fallback_return_value import fallback_return_value
 from packit.utils.repo import (
     get_namespace_and_repo_name,
     git_remote_url_to_https_url,
@@ -129,3 +130,41 @@ def test_ensure_str(inp, exp):
 )
 def test_relative_to(to, from_, exp):
     assert os.path.relpath(to, from_) == exp
+
+
+class TestFallbackReturnValue:
+    @pytest.mark.parametrize(
+        "raise_exception, decorator_exceptions",
+        [
+            pytest.param(ValueError, ValueError, id="raised"),
+            pytest.param(ValueError, KeyError, id="raised"),
+        ],
+    )
+    def test_fallback_return_value(self, raise_exception, decorator_exceptions):
+
+        fallback_value = "test_fallback_value"
+
+        @fallback_return_value(fallback_value, exceptions=decorator_exceptions)
+        def simple_function(exc=None):
+            """ Simple test function. """
+            if exc is not None:
+                raise exc
+            return 42
+
+        # `except` accepts both single exception or tuple of exceptions, to make testing easier
+        # we will transform also single exception to tuple
+        decorator_exceptions = (
+            decorator_exceptions
+            if isinstance(decorator_exceptions, tuple)
+            else (decorator_exceptions,)
+        )
+
+        if raise_exception:
+            if raise_exception in decorator_exceptions:
+                assert simple_function(raise_exception) == fallback_value
+            elif raise_exception not in decorator_exceptions:
+                with pytest.raises(raise_exception):
+                    simple_function(raise_exception)
+
+        elif not raise_exception:
+            assert simple_function() == 42
