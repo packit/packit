dc782db8c483374a1df1b0e82150584ae1690d97
Tomas Tomecek <ttomecek@redhat.com>
2021-02-15 11:26:33 +0100

    add unit tests for SourceGitGenerator
    `=== 1 passed in 0.06s ===`
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

---
+++ tests/unit/test_source_git_generator.py
@@ -0,0 +1,39 @@
+# Copyright Contributors to the Packit project.
+# SPDX-License-Identifier: MIT
+from pathlib import Path
+
+from flexmock import flexmock
+
+from packit.config import Config
+from packit.distgit import DistGit
+from packit.local_project import LocalProject
+from packit.source_git import SourceGitGenerator, CentOSDistGit
+from packit.utils.repo import create_new_repo
+
+
+def test_centos_conf(cronie, tmp_path: Path):
+    """ make sure the centos-specific configuration is correct """
+    source_git_path = tmp_path.joinpath("cronie-sg")
+    # create src-git
+    source_git_path.mkdir()
+    create_new_repo(source_git_path, [])
+    sgg = SourceGitGenerator(
+        LocalProject(working_dir=source_git_path),
+        Config(),
+        dist_git_path=cronie,
+        upstream_ref="cronie-1.5.2",
+        centos_package="cronie",
+    )
+
+    dg = sgg.dist_git
+    assert isinstance(dg, CentOSDistGit)
+
+    flexmock(
+        DistGit,
+        download_upstream_archive=lambda: cronie / "SOURCES" / "cronie-1.5.2.tar.gz",
+    )
+    assert sgg.primary_archive == cronie / "SOURCES" / "cronie-1.5.2.tar.gz"
+
+    assert dg.absolute_source_dir == cronie / "SOURCES"
+    assert dg.absolute_specfile_dir == cronie / "SPECS"
+    assert dg.absolute_specfile_path == cronie / "SPECS" / "cronie.spec"
