b7dbc4bd73c3463a44245ea9dae2c70f8431895e
Frantisek Lachman <flachman@redhat.com>
2022-03-16 09:46:01 +0100

    Get rid of flexmock.new_instances not to affect other tests
    Mocking of new_instances can collide with other
    manipulations of the class in different tests.
    Tests result should not be dependent on run of other tests.
    
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- packit/utils/bodhi.py
+++ packit/utils/bodhi.py
@@ -15,6 +15,13 @@ def get_bodhi_client(fas_username: str = None, fas_password: str = None) -> Bodh
     Bodhi will prompt for username and password
     if the values are not set and the session is not cached.
     (The session is stored as `~/.fedora/openidbaseclient-sessions.cache`.)
+
+    Note for tests:
+    * Don't mock the Bodhi instantiation via
+      `flexmock(bodhi.client.bindings.BodhiClient).new_instances(bodhi_instance_mock)`
+      what can affect other tests.
+    * Mock this function instead -- for example:
+      `flexmock(aliases).should_receive("get_bodhi_client").and_return(bodhi_instance_mock)`
     """
     logger.debug(
         f'Initialisation of the Bodhi client: FAS user {fas_username or "not configured"}; '
