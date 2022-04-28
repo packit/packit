c7207318fc2704ebfbe850b979fe3dbfa12074cc
Matej Focko <mfocko@redhat.com>
2022-04-28 15:48:56 +0200

    feat(copr): add bump-version to script generator
    When submitting SRPM build to Copr, we should be able to disable bumping
    of the version and release field.
    
    Related to #1181
    
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- packit/utils/source_script.py
+++ packit/utils/source_script.py
@@ -10,6 +10,7 @@ def create_source_script(
     merge_pr: Optional[bool] = True,
     target_branch: Optional[str] = None,
     job_config_index: Optional[int] = None,
+    bump_version: bool = True,
 ):
     options = []
     if ref:
@@ -20,6 +21,8 @@ def create_source_script(
             options += ["--target-branch", target_branch]
     if job_config_index is not None:
         options += ["--job-config-index", str(job_config_index)]
+    if not bump_version:
+        options += ["--no-bump"]
 
     options += [url]
     return COPR_SOURCE_SCRIPT.format(options=" ".join(options))
