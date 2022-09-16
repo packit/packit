6f852fb56c6dba32479f4698287cc36268b423aa
Frantisek Lachman <flachman@redhat.com>
2022-09-16 08:38:27 +0200

    Do not use PackitTest super class for requre tests
    It looks like it does not handle temp files correctly.
    
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- tests_recording/test_api.py
+++ tests_recording/test_api.py
@@ -1,20 +1,26 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
+import unittest
+from pathlib import Path
+from subprocess import CalledProcessError, check_output
 
-from subprocess import check_output
+from packit.api import PackitAPI
+from packit.config import Config, get_package_config_from_repo
+from packit.distgit import DistGit
+from packit.exceptions import PackitException
+from packit.local_project import LocalProject
+from packit.upstream import Upstream
 from requre.cassette import DataTypes
+from requre.helpers.files import StoreFiles
+from requre.helpers.simple_object import Simple
+from requre.modules_decorate_all_methods import (
+    record_git_module,
+    record_requests_module,
+    record_tempfile_module,
+)
 from requre.online_replacing import (
     apply_decorator_to_all_methods,
     replace_module_match,
-)
-from requre.helpers.simple_object import Simple
-from requre.helpers.files import StoreFiles
-from packit.api import PackitAPI
-from tests_recording.testbase import PackitTest
-from requre.modules_decorate_all_methods import (
-    record_requests_module,
-    record_tempfile_module,
-    record_git_module,
 )
 
 
@@ -39,14 +45,74 @@ from requre.modules_decorate_all_methods import (
         what="copr.v3.helpers.config_from_file", decorate=Simple.decorator_plain()
     )
 )
-class ProposeUpdate(PackitTest):
+class ProposeUpdate(unittest.TestCase):
     def cassette_setup(self, cassette):
         cassette.data_miner.data_type = DataTypes.Dict
 
     def setUp(self):
         super().setUp()
-        self.configure_git()
+        self.set_git_user()
         self._api = None
+        self._static_tmp = None
+        self._config = None
+        self._project = None
+        self._project_url = "https://github.com/packit/requre"
+        self._project_specfile_path = Path("fedora", "python-requre.spec")
+        self._pc = None
+        self._dg = None
+        self._lp = None
+        self._upstream = None
+
+    @staticmethod
+    def set_git_user():
+        try:
+            check_output(["git", "config", "--global", "-l"])
+        except CalledProcessError:
+            check_output(
+                ["git", "config", "--global", "user.email", "test@example.com"]
+            )
+            check_output(["git", "config", "--global", "user.name", "Tester"])
+
+    @property
+    def config(self):
+        if not self._config:
+            try:
+                self._config = Config.get_user_config()
+            except PackitException:
+                self._config = Config()
+        return self._config
+
+    @property
+    def project(self):
+        if not self._project:
+            self._project = self.config.get_project(url=self._project_url)
+        return self._project
+
+    @property
+    def pc(self):
+        if not self._pc:
+            self._pc = get_package_config_from_repo(project=self.project)
+        if not self._pc:
+            raise RuntimeError("Package config not found.")
+        return self._pc
+
+    @property
+    def dg(self):
+        if not self._dg:
+            self._dg = DistGit(self.config, self.pc)
+        return self._dg
+
+    @property
+    def lp(self):
+        if not self._lp:
+            self._lp = LocalProject(git_project=self.project)
+        return self._lp
+
+    @property
+    def upstream(self):
+        if not self._upstream:
+            self._upstream = Upstream(self.config, self.pc, local_project=self.lp)
+        return self._upstream
 
     @property
     def api(self):
@@ -59,6 +125,10 @@ class ProposeUpdate(PackitTest):
             self.api._up = self.upstream
             self.api._dg = self.dg
         return self._api
+
+    @property
+    def project_specfile_location(self):
+        return self.lp.working_dir / self._project_specfile_path
 
     def check_version_increase(self):
         """Bump version in specfile and tag it with the new version.
