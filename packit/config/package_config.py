439d6ac687abd9577c2b1a607a2fbc095bbbbc92
Jiri Popelka <jpopelka@redhat.com>
2023-03-01 16:35:40 +0100

    Rename find_remote_packit_yaml -> find_remote_package_config
    packit.yaml is only one of the names the package config can have

--- packit/config/package_config.py
+++ packit/config/package_config.py
@@ -247,11 +247,21 @@ def get_local_package_config(
     )
 
 
-def find_remote_packit_yaml(
+def find_remote_package_config(
     project: GitProject,
     ref: Optional[str] = None,
 ) -> Optional[str]:
-    package_config_path = None
+    """
+    Check if there's a package config file
+    in the given project (top-level directory only).
+
+    Args:
+        project: ogr Git-project object.
+        ref: Optional ref at which the config should be searched for.
+
+    Returns:
+        Name of the found config file or None if there's no such file.
+    """
 
     try:
         candidates = set(project.get_files(ref=ref, recursive=False))
@@ -263,7 +273,7 @@ def find_remote_packit_yaml(
         return None
 
     try:
-        package_config_path = (candidates & CONFIG_FILE_NAMES).pop()
+        package_config_name = (candidates & CONFIG_FILE_NAMES).pop()
     except KeyError:
         logger.warning(
             f"No config file ({CONFIG_FILE_NAMES}) found on ref {ref!r} "
@@ -272,11 +282,11 @@ def find_remote_packit_yaml(
         return None
 
     logger.debug(
-        f"Found a config file {package_config_path!r} "
+        f"Found a config file {package_config_name!r} "
         f"on ref {ref!r} "
         f"of the {project.full_repo_name!r} repository."
     )
-    return package_config_path
+    return package_config_name
 
 
 def get_package_config_from_repo(
@@ -299,7 +309,7 @@ def get_package_config_from_repo(
     """
     if not (
         package_config_path := package_config_path
-        or find_remote_packit_yaml(project, ref)
+        or find_remote_package_config(project, ref)
     ):
         return None
 
