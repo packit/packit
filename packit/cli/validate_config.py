e3bac6b0e57facfd537933b52b0561578c3b25b7
Jiri Popelka <jpopelka@redhat.com>
2023-04-29 13:18:43 +0200

    LocalProjectParameter can now also be a path to a file
    in that case we take its parent to build & return LocalProject out of it.
    
    This is needed for the validate-config pre-commit hook,
    which calls the 'packit validate-config' with a path to
    the config file.

--- packit/cli/validate_config.py
+++ packit/cli/validate_config.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 @cover_packit_exception
 def validate_config(path_or_url: LocalProject):
     """
-    Validate PackageConfig validation.
+    Validate PackageConfig.
 
     \b
     - checks missing values
@@ -32,7 +32,6 @@ def validate_config(path_or_url: LocalProject):
 
     PATH_OR_URL argument is a local path or a URL to a git repository with packit configuration file
     """
-    # we use PackageConfig.load_from_dict for the validation, hence we don't parse it here
     output = PackitAPI.validate_package_config(path_or_url.working_dir)
     logger.info(output)
     # TODO: print more if config.debug
