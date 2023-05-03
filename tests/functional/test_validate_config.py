9604c56303fa377ee631dfaa18fcfc1f1b65dffa
Jiri Popelka <jpopelka@redhat.com>
2023-05-03 18:21:14 +0200

    PackageConfigValidator now raises exception if validate() fails
    Previously, there was no simple flag/way how to know
    whether the validation failed or passed.
    In both cases we just returned a string
    - in case of failed validation: with errors
    - in case of successful validation: with a success string
    As a consequence the return value of 'packit config-validation' was always 0.
    
    With this change, the return value of 'packit config-validation'
    is 2 in case of failed validation.

--- tests/functional/test_validate_config.py
+++ tests/functional/test_validate_config.py
@@ -6,11 +6,11 @@ Functional tests the validate-config command
 """
 
 from packit.utils.commands import cwd
-from tests.functional.spellbook import call_real_packit
+from tests.functional.spellbook import call_real_packit_and_return_exit_code
 
 
 def test_srpm_command_for_path(upstream_or_distgit_path, tmp_path):
     with cwd(tmp_path):
-        call_real_packit(
+        call_real_packit_and_return_exit_code(
             parameters=["--debug", "validate-config", str(upstream_or_distgit_path)]
         )
