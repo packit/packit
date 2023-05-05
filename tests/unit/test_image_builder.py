f8377b2cfb0614393da4a6319148707992962ee9
Tomas Tomecek <ttomecek@redhat.com>
2023-05-05 14:10:39 +0200

    image builder: raise exc when cannot obtain token
    the subsequent API request would fail badly
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- tests/unit/test_image_builder.py
+++ tests/unit/test_image_builder.py
@@ -1,14 +1,17 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
+import pytest
 from flexmock import flexmock
+
+from packit.exceptions import PackitException
 from packit.vm_image_build import ImageBuilder
 
 
 def test_create_image():
     """Test creating an image"""
-    ib = ImageBuilder("foo-token")
     # auth is tested in tests_recording
-    flexmock(ib).should_receive("refresh_auth").and_return(None)
+    flexmock(ImageBuilder).should_receive("refresh_auth").and_return(None)
+    ib = ImageBuilder("foo-token")
     payload = {
         "image_name": "mona-lisa",
         "distribution": "rhel-90",
@@ -52,3 +55,14 @@ def test_create_image():
         "https://copr.fedoraproject.org/coprs/foo/bar/",
     )
     assert response == "foo-baz-bar"
+
+
+def test_refresh_fails():
+    """Test that refreshing with no token fails properly"""
+    flexmock(ImageBuilder).should_receive("_get_access_token").and_return(None)
+    with pytest.raises(PackitException) as exc:
+        ImageBuilder(None)
+    assert (
+        "Unable to obtain access token. You may need to regenerate the refresh token."
+        in str(exc)
+    )
