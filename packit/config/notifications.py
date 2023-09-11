ec514638660530d59c35c3ea3e60dffaa05872df
Laura Barcziova <lbarczio@redhat.com>
2023-09-11 16:15:56 +0200

    Add failure_comment_message to notifications config
    Related to packit/packit-service#1911

--- packit/config/notifications.py
+++ packit/config/notifications.py
@@ -1,5 +1,6 @@
 # Copyright Contributors to the Packit project.
 # SPDX-License-Identifier: MIT
+from typing import Optional
 
 
 class PullRequestNotificationsConfig:
@@ -12,5 +13,10 @@ class PullRequestNotificationsConfig:
 class NotificationsConfig:
     """Configuration of notifications."""
 
-    def __init__(self, pull_request: PullRequestNotificationsConfig):
-        self.pull_request = pull_request
+    def __init__(
+        self,
+        pull_request: Optional[PullRequestNotificationsConfig] = None,
+        failure_comment_message: Optional[str] = None,
+    ):
+        self.pull_request = pull_request or PullRequestNotificationsConfig()
+        self.failure_comment_message = failure_comment_message
