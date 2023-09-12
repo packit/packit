c763a0cb5de85eb62bbb841ccb8c6141f742b1fd
Laura Barcziova <lbarczio@redhat.com>
2023-09-12 12:21:34 +0200

    Change configuration schema for failure comment
    Instead of having one value failure_comment_message, create a nested one
    failure_comment.message so that failure_comment can be extended in the future
    if needed. Related to packit/packit-service#1911

--- packit/config/notifications.py
+++ packit/config/notifications.py
@@ -10,13 +10,20 @@ class PullRequestNotificationsConfig:
         self.successful_build = successful_build
 
 
+class FailureCommentNotificationsConfig:
+    """Configuration of the failure comment."""
+
+    def __init__(self, message: Optional[str] = None):
+        self.message = message
+
+
 class NotificationsConfig:
     """Configuration of notifications."""
 
     def __init__(
         self,
         pull_request: Optional[PullRequestNotificationsConfig] = None,
-        failure_comment_message: Optional[str] = None,
+        failure_comment: Optional[FailureCommentNotificationsConfig] = None,
     ):
         self.pull_request = pull_request or PullRequestNotificationsConfig()
-        self.failure_comment_message = failure_comment_message
+        self.failure_comment = failure_comment or FailureCommentNotificationsConfig()
