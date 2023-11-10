a4bde4db5af67003489004caab3bd526659d4f56
Maja Massarini <mmassari@redhat.com>
2023-11-10 10:43:01 +0100

    Check proposed and current version before release
    On stable branches we should avoid doing major updates releases.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -64,6 +64,7 @@ def package_config_mock():
         patch_generation_patch_id_digits=4,
         is_sub_package=False,
         pkg_tool=None,
+        version_update_mask="",
     )
     mock.should_receive("get_all_files_to_sync").and_return([])
     mock.should_receive("get_package_names_as_env").and_return({})
