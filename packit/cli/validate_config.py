54c53f619102fab0a7c63eae20e57fd8d31d171d
Laura Barcziova <lbarczio@redhat.com>
2024-02-02 14:37:52 +0100

    validate-config: Check monitoring for pull-from-upstream
    Add 2 checks regarding monitoring of the package by
    Upstream Release Monitoring if pull-from-upstream job
    is present in the config.
    
    Fixes #2224

--- packit/cli/validate_config.py
+++ packit/cli/validate_config.py
@@ -21,17 +21,24 @@ logger = logging.getLogger(__name__)
 
 @click.command("validate-config", context_settings=get_context_settings())
 @click.argument("path_or_url", type=LocalProjectParameter(), default=os.path.curdir)
+@click.option(
+    "--offline",
+    default=False,
+    is_flag=True,
+    help="Do not make remote API calls requiring network access.",
+)
 @cover_packit_exception
-def validate_config(path_or_url: LocalProject):
+def validate_config(path_or_url: LocalProject, offline: bool):
     """
     Validate PackageConfig.
 
     \b
     - checks missing values
     - checks incorrect types
+    - checks whether monitoring is enabled if 'pull_from_upstream` is used
 
     PATH_OR_URL argument is a local path or a URL to a git repository with packit configuration file
     """
-    output = PackitAPI.validate_package_config(path_or_url.working_dir)
+    output = PackitAPI.validate_package_config(path_or_url.working_dir, offline)
     logger.info(output)
     # TODO: print more if config.debug
