f1e3c20ff015283ea0fcc8345eba761bc0e89738
Nikola Forró <nforro@redhat.com>
2025-02-11 21:08:57 +0100

    Fix downloads from misconfigured lookaside cache servers
    Signed-off-by: Nikola Forró <nforro@redhat.com>

--- packit/base_git.py
+++ packit/base_git.py
@@ -643,13 +643,13 @@ class PackitRepositoryBase:
             try:
                 with requests.get(
                     url,
-                    headers={"User-Agent": user_agent},
+                    headers={"User-Agent": user_agent, "Accept-Encoding": "identity"},
                     timeout=HTTP_REQUEST_TIMEOUT,
                     stream=True,
                 ) as response:
                     response.raise_for_status()
                     with open(source_path, "wb") as f:
-                        for chunk in response.iter_content(chunk_size=8192):
+                        for chunk in response.raw.stream(8192, decode_content=False):
                             f.write(chunk)
             except requests.exceptions.RequestException as e:
                 msg = f"Failed to download source from {url}"
