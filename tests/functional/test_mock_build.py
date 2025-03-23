ad53ab22e5141f5f3dfbf5cc292b4a456360ab82
Amarach0rdor18 <amarachiordor2@gmail.com>
2025-03-23 00:52:35 -0400

    Moved test_mock_build.py from unit to functional tests, switched to call_real_packit(), added cwd, included -r <target>, and added a fixture for missing sources

---
+++ tests/functional/test_mock_build.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import pytest
+
+from tests.functional.spellbook import call_real_packit
+
+def test_build_in_mock_default_resultdir(upstream_and_remote):
+    cwd, _ = upstream_and_remote  
+
+    call_real_packit(
+        parameters=["--debug", "build", "in-mock", "-r", "fedora-rawhide-x86_64"],
+        cwd=cwd,  
+    )
+
+    rpm_paths = list(cwd.glob("noarch/*.rpm"))  
+
+    assert rpm_paths, "No RPMs were found in the expected directory!"
+    assert all(rpm_path.exists() for rpm_path in rpm_paths), "Some RPM files do not exist!"
+
