1f2fcaa9de1fef362dfc502585653fb207f2c828
Amarach0rdor18 <amarachiordor2@gmail.com>
2025-03-23 01:09:55 -0400

    Moved test_mock_build.py from unit to functional tests, switched to call_real_packit(), added cwd, included -r <target>, and added a fixture for missing sources

--- tests/unit/test_mock_build.py
+++
@@ -1,6 +0,0 @@
-# Copyright Contributors to the Packit project.
-# SPDX-License-Identifier: MIT
-
-def test_build_in_mock_default_resultdir():
-    result = run_packit_command(["build", "in-mock"])
-    assert os.path.exists("./some_expected_file")
