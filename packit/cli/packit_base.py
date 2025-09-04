4651848dca55d919b3d2bcaeb13299125dc5e5aa
kayodegigz <kayogunrinde@gmail.com>
2025-09-04 11:50:57 +0100

    second review from fixes

--- packit/cli/packit_base.py
+++ packit/cli/packit_base.py
@@ -11,7 +11,6 @@ from packit.cli.config import config
 from packit.cli.create_update import create_update
 from packit.cli.dist_git import dist_git
 from packit.cli.init import init
-from packit.cli.init_local_yaml import init_local_yaml
 from packit.cli.prepare_sources import prepare_sources
 from packit.cli.propose_downstream import propose_downstream, pull_from_upstream
 from packit.cli.push_updates import push_updates
@@ -88,7 +87,7 @@ packit_base.add_command(dist_git)
 packit_base.add_command(scan_in_osh)
 packit_base.add_command(config)
 packit_base.add_command(test)
-packit_base.add_command(init_local_yaml)
+# packit_base.add_command(init_local_yaml)
 
 if __name__ == "__main__":
     packit_base()
